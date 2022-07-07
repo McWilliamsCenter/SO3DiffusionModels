86979401f4fa51c34b0174ac00b00f2550c40052
EiffL <fr.eiffel@gmail.com>
2022-07-07 22:48:56 +0200

    Adding quaternion examples

--- so3dm/quaternions.py
+++ so3dm/quaternions.py
@@ -10,14 +10,13 @@ def q_exp(q):
     Returns:
       The quaternion exponential of q.
     """
-    a = q[:, 0]
-    v = q[:, 1:]
+    a = q[..., 0]
+    v = q[..., 1:]
     exp_scalar = jnp.exp(a)
-    v_norm = jnp.linalg.norm(v, axis=1)
-    v_norm = v_norm.T
+    v_norm = jnp.linalg.norm(v, axis=-1)
     cos_v = jnp.cos(v_norm)
     sin_v = jnp.sin(v_norm)
-    sin_v_over_v_norm = jnp.divide(sin_v, v_norm)
+    sin_v_over_v_norm = sin_v / v_norm
     scalar_res = exp_scalar*cos_v
-    vec_res = sin_v_over_v_norm[:, jnp.newaxis]*exp_scalar[:, jnp.newaxis]*v
-    return jnp.column_stack((scalar_res, vec_res))
+    vec_res = sin_v_over_v_norm*exp_scalar*v
+    return jnp.concatenate((scalar_res[..., jnp.newaxis], vec_res), axis=-1)
