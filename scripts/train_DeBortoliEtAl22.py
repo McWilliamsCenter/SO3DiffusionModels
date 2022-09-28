159908826f51b40e01276d792cfc61e89acfc8ef
EiffL <fr.eiffel@gmail.com>
2022-09-28 23:47:42 +0200

    solves a small issue with saving samples

--- scripts/train_DeBortoliEtAl22.py
+++ scripts/train_DeBortoliEtAl22.py
@@ -192,9 +192,9 @@ def main(_):
   for i in tqdm(range(FLAGS.test_nsamples// (cfg.batch_size * M) + 1)):
     x = get_samples(next(rng_seq))
     samples.append(x)
-  samples = jnp.stack(samples, axis=0)[:FLAGS.test_nsamples]
+  x = jnp.stack(samples, axis=0)[:FLAGS.test_nsamples]
     
-  with open(output_dir + FLAGS.dataset + '_' + str(len(x)) + ".npy", "wb") as f:
+  with open(output_dir + FLAGS.dataset + '_' + str(FLAGS.test_nsamples) + ".npy", "wb") as f:
     # Converting to quaternions
     q = jax.vmap(lambda R: SO3.from_matrix(R).wxyz)(x)
     onp.save(f, q)
